468dc480d4201aee27fec1df0e00a963021de7bc
Chudovisha <lemigi123@gmail.com>
2021-08-31 08:30:55 +0000

    code

--- js/index.js
+++ js/index.js
@@ -80,6 +80,21 @@ for (let e of document.querySelectorAll('input[type="range"].slider-progress')) 
   e.addEventListener('input', () => e.style.setProperty('--value', e.value));
 }
 
+var result = Number(document.getElementById("result").value.replace('%',''))
+document.getElementById("btnminus").onclick = function(){
+  if (result>0.1) {
+    result-=0.1;
+  }
+  document.getElementById("result").innerHTML = result.toFixed(1) + '%';
+}
+
+document.getElementById("btnplus").onclick = function(){
+  if (result<99.9) {
+    result+=0.1;
+  }
+  document.getElementById("result").innerHTML = result.toFixed(1) + '%';
+}
+
 
 
 
